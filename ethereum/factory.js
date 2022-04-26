475a154cd9bee0ca94f9eb2aeca166094f8dfe25
Wannes Dieltiens <wannesdieltiens@gmail.com>
2022-04-26 13:16:10 +0200

    added campaign info fetching

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x0f329f5A28B0502F14251724e98f7346235F28f2"
+  "0xd0ffF7D8319a106A34194C0227d22Ff95784Bce7"
 );
 
 export default instance;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,33 @@
+import { assignNewMochaID } from 'mocha/lib/utils';
 import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+
+    const summary = await campaign.methods.getSummary().call();
+    //result is an object!
+    return { 
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+     };
+  }
+
+  renderCards() {
+    const items = 
+  }
+
   render() {
-    return <h3>Campaign Show</h3>;
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+      </Layout>
+    );
   }
 }
 
